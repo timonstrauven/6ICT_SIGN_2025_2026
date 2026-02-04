b8b94b67a856d386c891017a139395ed3ce16c8d
timonstrauven <stti111108@leerling.mosa-rt.be>
2026-02-04 11:24:01 +0100

    auto update S(date '+%Y-%m-%d %H:%M:%S')

--- data_schrijven_naar_OneDrive.py
+++ data_schrijven_naar_OneDrive.py
@@ -14,7 +14,7 @@ while True:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9/5) + 32
         humidity = dhtDevice.humidity
-        f.open("DHT11_cloud.csv")
+        f.open("DHT11_cloud.csv", "a")
         f.write(f"{temperature_c}, {humidity} \n")
         subprocess.run(["/bin/bash", "/home/rpi/autopush.sh"])
 
