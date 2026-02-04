c59c7efbe7250f5892092d29257b08624926482c
timonstrauven <stti111108@leerling.mosa-rt.be>
2026-02-04 11:18:45 +0100

    auto update S(date '+%Y-%m-%d %H:%M:%S')

--- data_schrijven_naar_OneDrive.py
+++ data_schrijven_naar_OneDrive.py
@@ -1,3 +1,27 @@
 import subprocess
 
-subprocess.run(["/bin/bash", "/home/rpi/autopush.sh"])
+
+import time
+import board
+import adafruit_dht
+
+dhtDevice = adafruit_dht.DHT11(board.D18) 
+
+f=open("DHT11_cloud.csv" , "w")
+f.write("Temp, Humidity \n")
+while True:
+    try:        
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9/5) + 32
+        humidity = dhtDevice.humidity
+        f.write(f"{temperature_c}, {humidity} \n")
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+    subprocess.run(["/bin/bash", "/home/rpi/autopush.sh"])
+  
